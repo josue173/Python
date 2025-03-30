d781b7f43cfa3a4a56bcd68e0a2e4b7d6fd34007
josue173 <josue.perez04@outlook.com>
2025-03-29 22:44:44 -0600

    Add main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# Comentario en Python
+# Mi Python
+print("Hello World!")
+print('Hello World!')
+"""
+Primer comentario multilinea 
+:D
+"""
+'''
+Segundo comentario multilinea
+:D
+'''
+print(type('HI'))
