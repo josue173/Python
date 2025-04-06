4ebaaa2f16034ee8b3c48e0ee79e4a847bf1f1df
josue173 <josue.perez04@outlook.com>
2025-04-06 13:02:47 -0600

    feat: operadores lógicos

--- operadores.py
+++ operadores.py
@@ -1,4 +1,4 @@
-# OPERADORES
+# OPERADORES ARITMETICOS
 
 print(25+3)
 print(7-3)
@@ -11,3 +11,24 @@ print(3**4) # Potenciación
 
 print("Hola" + "Mundo")
 print("Hola " * 5)
+
+# OPERADORES RELACIONALES
+
+print(3>4)
+print(3<4)
+print(3<=4)
+print(3>=4)
+print(3 == 4)
+print(3!=4)
+print(3>4 == 2)
+# Compara los valores ASCII, no la cantidad de caracteres
+print("Hola" > "Python")
+print("HOla" != "Hola")
+print("Hola" == "Hola")
+print("a" > "b")
+
+# OPERADORES LÓGICOS
+
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 or "Hola" > "Python")
+# print(3 > 4 not "Hola" > "Python")
