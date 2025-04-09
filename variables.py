3acdc5ad2cc15fa426dfe571853b52891a7daeda
josue173 <josue.perez04@outlook.com>
2025-04-08 21:37:40 -0600

    feat: cambios en Windows

--- variables.py
+++ variables.py
@@ -1,6 +1,10 @@
 variable = "Hola"
 variable = 3
+texto = "Variable tipo string"
+nombre, apellido, edad = "Antony", "Toribio", 22
 print(variable)
 
 variable_boleana = True
 print(variable_boleana)
+print(len(texto))
+print("Mi nombre es ", nombre, " ", apellido, " y tengo ", edad, "años")
