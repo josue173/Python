31311caf52605ed21afcc79dabbaf3b05ac81b07
josue173 <josue.perez04@outlook.com>
2025-04-13 20:26:43 -0600

    feat: inicio con tuplas

---
+++ tuplas.py
@@ -0,0 +1,11 @@
+tupla = tuple()
+otra_tupla = ()
+
+tupla = (42,534.523, "Antony", "Toribio")
+
+print(tupla)
+print(type(tupla))
+print(tupla[0])
+
+print(tupla.count("Antony"))
+print(tupla.index("Antony"))
