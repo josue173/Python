dd7ef548817c8fbbae7e2a80b94a5768b0b33414
josue173 <josue.perez04@outlook.com>
2025-06-23 21:42:04 -0600

    feat: inicio de condicionales

---
+++ condicionales.py
@@ -0,0 +1,20 @@
+mi_condicion = True
+
+if mi_condicion:
+  print("Primera condicional culeros")
+
+print("Me salí del if culos")
+
+mi_condicion = 5*2
+
+if mi_condicion == 10:
+  print("PRIMER IF")
+else:
+  print ('MAYOR')
+
+print("Continua")
+
+if mi_condicion > 10:
+  print("Segundo IF")
+
+print("Continua 2")
