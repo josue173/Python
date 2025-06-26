b5c00cb8ad26090e49ff38acf58c863bcc2f8e5a
josue173 <josue.perez04@outlook.com>
2025-06-25 22:11:49 -0600

    feat: fin condicionales

--- condicionales.py
+++ condicionales.py
@@ -1,20 +1,30 @@
 mi_condicion = True
 
 if mi_condicion:
-  print("Primera condicional culeros")
+  print("Primera condicional")
 
-print("Me salí del if culos")
+print("Me salí del if")
 
-mi_condicion = 5*2
+mi_condicion = 5*3
 
 if mi_condicion == 10:
-  print("PRIMER IF")
+  print("Es 10")
+elif mi_condicion > 10 and mi_condicion < 20:
+  print("Es menor o igual que 10 y menor a 20")
+elif mi_condicion == 25:
+  print("Es 5")
 else:
-  print ('MAYOR')
-
-print("Continua")
-
-if mi_condicion > 10:
-  print("Segundo IF")
+  print("Es menor o igual que 10 o mayor o igual que 20")
 
 print("Continua 2")
+
+mi_string = ""
+
+if not mi_string:
+  print("La cadena está vacía")
+
+if mi_string:
+  print("Hay información")
+
+if mi_string == "TEXTO":
+  print("Coincidencia")
