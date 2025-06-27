0f1c1e005b4f077af947c2dd7a61f2c6aaa3c8ba
josue173 <josue.perez04@outlook.com>
2025-06-26 22:41:58 -0600

    feat: inicio con bucles

---
+++ bucles.py
@@ -0,0 +1,11 @@
+
+mi_condicion = 0
+
+while mi_condicion < 10:
+  print("Entra")
+  print(mi_condicion)
+  mi_condicion += 1
+else:
+  print("Mi condición es mayor o igual que 10")
+
+print("La ejecución continúa")
