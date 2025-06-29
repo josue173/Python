d66b4f527dbe30ed7c1399a6841efa465ff078dd
josue173 <josue.perez04@outlook.com>
2025-06-29 15:31:50 -0600

    feat: inicio funciones

---
+++ funciones.py
@@ -0,0 +1,16 @@
+def mi_funcion():
+  print("Primera función")
+
+mi_funcion()
+
+def sumar_dos_numeros(primer_numero, segundo_numero):
+  print(primer_numero + segundo_numero)
+
+sumar_dos_numeros(3,44242.15)
+sumar_dos_numeros('333', '321321')
+
+def retornar_dos_valores(uno, dos):
+  print(uno+dos)
+  return uno + dos
+
+retornar_dos_valores(3,4)
