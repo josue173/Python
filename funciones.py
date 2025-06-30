b266bea6103786faebf7135cd193ddbbc3f05e08
josue173 <josue.perez04@outlook.com>
2025-06-29 18:07:27 -0600

    feat: fin funciones

--- funciones.py
+++ funciones.py
@@ -10,7 +10,25 @@ sumar_dos_numeros(3,44242.15)
 sumar_dos_numeros('333', '321321')
 
 def retornar_dos_valores(uno, dos):
-  print(uno+dos)
   return uno + dos
 
-retornar_dos_valores(3,4)
+resultado = retornar_dos_valores(3,4)
+print(resultado)
+
+def print_nombre(nombre, apellido):
+  print(f'{nombre} {apellido}')
+
+print_nombre("Antony", "Toribio")
+print_nombre(apellido = "Toribio", nombre = "Antony")
+
+def print_nombre_por_defecto(nombre, apellido, alias = 'Sin datos'):
+  print(f'{nombre} {apellido} {alias}')
+
+print_nombre_por_defecto("Nombre", "Apellido")
+print_nombre_por_defecto("Nombre", "Apellido", 'Con datos')
+
+def print_textos(*textos):
+  for texto in textos:
+    print(texto)
+
+print_textos('Hola','¿Cómo estás?')
